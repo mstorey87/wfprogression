cd0dbdbd07f4b7d7cc0a7933a04f258eb647c528
mstorey87 <michaelstorey@y7mail.com>
2024-07-25 14:59:47 +1000

    add landsat path and row data

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' Landsat tiles for Australia
+#'
+#' A subset of landsat tiles with Path and Row that intersect Australia
+#' @format ## `dat.landsat.pathrow`
+#' An sf object with 404 rows and 4 columns:
+#' \describe{
+#'   \item{PATH}{Landsat Path Number}
+#'   \item{ROW, iso3}{Landsat Row number}
+#'   \item{WRSPR}{Path and Row string}
+#'   ...
+#' }
+#' @source <https://www.usgs.gov/media/files/landsat-wrs-2-descending-path-row-shapefile>
+"dat.landsat.pathrow"
