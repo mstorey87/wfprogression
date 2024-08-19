87050e75006e66f97c8c3559b51259e9487292cf
mstorey87 <michaelstorey@y7mail.com>
2024-08-19 14:09:15 +1000

    Update himawari function

--- R/fire_download_himiwari.R
+++ R/fire_download_himiwari.R
@@ -15,7 +15,7 @@ fire_download_himiwari <- function(fire_bbox,df_download,bands=c("B07","B04","B0
   df_download <- df_download %>%
     #filter and arrange bands by user input
     dplyr::filter(band %in% bands)%>%
-    dplyr::arrange(factor(band,levels=bands))
+    dplyr::arrange(factor(band,levels=rev(bands)))
 
   times_unique <- unique(df_download$datetimelocal_chr)
 
@@ -49,7 +49,7 @@ fire_download_himiwari <- function(fire_bbox,df_download,bands=c("B07","B04","B0
     }else{
 
       dat.i <- dat.i %>%
-        dplyr::filter(band==bands)
+        dplyr::filter(band==bands[1])
 
       b <- tidync::tidync(dat.i$path_download)
       b.local <- b  %>%
@@ -72,7 +72,7 @@ fire_download_himiwari <- function(fire_bbox,df_download,bands=c("B07","B04","B0
     #set Himawari crs
     terra::crs(r) <- "+proj=geos +lon_0=140.7 +h=35785863 +x_0=0 +y_0=0 +a=6378137 +b=6356752.3 +units=m +no_defs"
     outtif <- paste0(dest_folder,"\\",dat.i$datetimelocal_chr[1],"_",
-                     dat.i$satellite[1],"_",length(names(r)),"bands.tif")
+                     dat.i$satellite[1],"_",paste0(bands,collapse = ""),".tif")
     terra::writeRaster(r,outtif)
 
   }
