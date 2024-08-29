bb07781119b1d237f672438f5ce290b06ea1606f
mstorey87 <michaelstorey@y7mail.com>
2024-08-29 13:06:11 +1000

    create postgres search functions

---
+++ R/fire_search_progs.R
@@ -0,0 +1,45 @@
+fire_search_progs <- function(fire_poly,start_date,end_date){
+
+
+  #connect to database
+  DB <- DBI::dbConnect(RPostgres::Postgres(),
+                       dbname = "cermb_fires",
+                       user = "mstorey",
+                       password = "bushfires",
+                       host = "charus.ad.uow.edu.au",
+                       port = 5432)
+
+  #simplify fire polygon shape
+  fire_poly <- fire_poli %>%
+    sf::st_transform(4283) %>%
+    sf::st_concave_hull(ratio = 0.8)
+
+  #add times on to dates for searching
+  start_date <- paste0(start_date," 00:00:00")
+  end_date <- paste0(as.Date(end_date)+1," 00:00:00")
+
+  #estract geometry of fire as text
+  txt_geom <- st_as_text(x2$geometry,EWKT=T)
+
+  #create date part of query
+  txt_date <- paste0("datetime between '",start_date, "' AND '", end_date,"'")
+
+  #create query
+  myquery <- paste0("SELECT * FROM fires.progressions WHERE st_intersects(fires.progressions.geom,'",txt_geom,"') AND ", txt_date)
+
+  #run query and get results
+  x <- sf::st_read(dsn=DB,query=myquery)
+
+  DBI::dbDisconnect(DB)
+
+  if(nrow(x)==0){
+    print("no results")
+  }else{
+    print(paste0(nrow(x)," results"))
+    return(x)
+
+  }
+
+
+
+}
