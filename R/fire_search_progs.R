c4bf1222311dfd1819fab06124cf1abdc80a7c3d
mstorey87 <michaelstorey@y7mail.com>
2024-10-22 13:12:08 +1100

    Update search progressions functions to extract bbox geometry using st_geometry

--- R/fire_search_progs.R
+++ R/fire_search_progs.R
@@ -22,6 +22,8 @@ fire_search_progs <- function(fire_bbox,start_date,end_date){
 
   #simplify fire polygon shape for intersect
   #mainly useful when fire polygon is complex
+
+
   fire_bbox <- fire_bbox %>%
     sf::st_transform(4283) %>%
     sf::st_concave_hull(ratio = 0.8)
@@ -31,7 +33,7 @@ fire_search_progs <- function(fire_bbox,start_date,end_date){
   end_date <- paste0(as.Date(end_date)+1," 00:00:00")
 
   #extract geometry of fire as text
-  txt_geom <- sf::st_as_text(fire_bbox$geometry,EWKT=T)
+  txt_geom <- sf::st_as_text(st_geometry(dat.bbox),EWKT = T)
 
   #create date part of query
   txt_date <- paste0("dt_local between '",start_date, "' AND '", end_date,"'")
