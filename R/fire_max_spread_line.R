06e437e7a2e47cead6be836d5c856b9d27c207be
mstorey87 <michaelstorey@y7mail.com>
2024-11-14 20:31:16 +1100

    remove firename mutate

--- R/fire_max_spread_line.R
+++ R/fire_max_spread_line.R
@@ -81,8 +81,7 @@ fire_max_spread_line <- function(polygons,time_col,include_spots=F,include_backb
       res[[i]] <- dat.lines %>%
         mutate(lineid=dplyr::row_number(),
                start_time=unique(dat.cnvx.split[[i-1]]$time),
-               end_time=unique(dat.cnvx.split[[i]]$time),
-               firename=unique(dat.cnvx.split[[i-1]]$firename))
+               end_time=unique(dat.cnvx.split[[i]]$time))
     }
 
 
