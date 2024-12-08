6b0c429c85239af102e9fd1e3b036667a85cd27e
Michael Storey <mstorey@uow.edu.au>
2024-12-09 10:42:01 +1100

    Add roxygen for scan read code

--- R/fire_read_scan.R
+++ R/fire_read_scan.R
@@ -1,3 +1,12 @@
+#' Read a linescan with s3path
+#'
+#' @param s3name full https path or tif file name
+#'
+#' @return terra rast object
+#' @export
+#'
+#' @examples
+#' #
 fire_read_scan <- function(s3name){
 
   #if only the file name is input, add in the path
