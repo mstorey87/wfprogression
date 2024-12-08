4d76732316913697af72a223cc4ad90e62c8d9b5
Michael Storey <mstorey@uow.edu.au>
2024-12-09 10:30:33 +1100

    Update s3path

--- R/fire_read_scan.R
+++ R/fire_read_scan.R
@@ -2,8 +2,14 @@ fire_read_scan <- function(s3name){
 
   #if only the file name is input, add in the path
   if(!stringr::str_detect(s3name,"https")){
-    s3name <- paste0("https://s3.its.uow.edu.au/uow-rb01186-ros-model/scans.original.crs/",s3name,".tif")
+    s3name <- paste0("https://s3.its.uow.edu.au/uow-rb01186-ros-model/scans.original.crs/",s3name)
   }
+
+  if(!stringr::str_ends(s3name,".tif")){
+    s3name <- paste0(s3name,".tif")
+  }
+
+
 
   ras <- terra::rast(s3name)
 
