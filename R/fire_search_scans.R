27d772ca5124073119d41083f1586f68aa253473
Michael Storey <mstorey@uow.edu.au>
2025-01-15 11:27:45 +1100

    update search scans function to accept posixct times only

--- R/fire_search_scans.R
+++ R/fire_search_scans.R
@@ -1,15 +1,27 @@
 #' Search database for line scans
 #'
 #' @param fire_bbox A polygon, usually fire bounding box, to search for images
-#' @param start_date The first date for which to search for images YYYY-mm-dd
-#' @param end_date The last date for which to search for images YYYY-mm-dd
+#' @param start_time The first date for which to search for images YYYY-mm-dd
+#' @param end_time The last date for which to search for images YYYY-mm-dd
 #'
 #' @return sf object
 #' @export
 #'
 #' @examples
-#' #dat.scans.sf <- fire_search_scans(fire_bbox=dat.bbox,start_date=datestart,end_date=dateend)
-fire_search_scans <- function(fire_bbox=fire_bbox_polygon(),start_date="1990-01-01",end_date="2100-01-01"){
+#' #dat.scans.sf <- fire_search_scans(fire_bbox=dat.bbox,start_time=datestart,end_time=dateend)
+fire_search_scans <- function(fire_bbox=fire_bbox_polygon(),start_time,end_time){
+
+
+  checkmate::assert(stringr::str_detect(class(start_time)[1],"POSIXct"),"Error: times must be posixct")
+  checkmate::assert(stringr::str_detect(class(end_time)[1],"POSIXct"),"Error: times must be posixct")
+
+  #ensure times are local
+  my_tz <- wfprogression::fire_get_timezone(fire_bbox)
+  start_time <- lubridate::with_tz(start_time,tz=my_tz)
+  end_time <- lubridate::with_tz(end_time,tz=my_tz)
+
+
+
 
   #connect to database
   DB <- DBI::dbConnect(RPostgres::Postgres(),
@@ -28,16 +40,11 @@ fire_search_scans <- function(fire_bbox=fire_bbox_polygon(),start_date="1990-01-
        sf::st_concave_hull(ratio = 0.8)
 
 
-
-  #add times on to dates for searching
-  start_date <- paste0(start_date," 00:00:00")
-  end_date <- paste0(as.Date(end_date)+1," 00:00:00")
-
   #extract geometry of fire as text
   txt_geom <- sf::st_as_text(fire_bbox$geometry,EWKT=T)
 
   #create date part of query
-  txt_date <- paste0("datetimelocal between '",start_date, "' AND '", end_date,"'")
+  txt_date <- paste0("datetimelocal between '",format(start_time,format="%Y-%m-%d %H:%M:%S"), "' AND '", format(end_time,format="%Y-%m-%d %H:%M:%S"),"'")
 
   #create whole SQL query
   myquery <- paste0("SELECT * FROM fires.scanboundingboxes WHERE st_intersects(fires.scanboundingboxes.geom,'",txt_geom,"') AND ", txt_date)
