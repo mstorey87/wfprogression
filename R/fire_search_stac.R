d09d6a5648c85a46c7354fb56a9a81464c1e7706
Michael Storey <mstorey@uow.edu.au>
2025-01-15 12:38:24 +1100

    update search stac function to accept posixct

--- R/fire_search_stac.R
+++ R/fire_search_stac.R
@@ -1,24 +1,38 @@
 #' Search Geoscience Aus. STAC for Landsat and Sentinel 2 data
 #'
 #' @param fire_bbox A polygon, usually fire bounding box, to search for images
-#' @param start_date The first date for which to search for images YYYY-mm-dd
-#' @param end_date The last date for which to search for images YYYY-mm-dd
+#' @param start_time The first date for which to search for images YYYY-mm-dd
+#' @param end_time The last date for which to search for images YYYY-mm-dd
 #' @param collection_names Names of which Landsat and Sentinel 2 products to search for. Defaults to all products from Landsat 5 onwards.
 #'
 #' @return data frame with image paths
 #' @export
 #'
 #' @examples
-#' #dat.stac <- fire_search_stac(fire_bbox = dat.bbox,start_date = datestart,end_date = dateend)
+#' #dat.stac <- fire_search_stac(fire_bbox = dat.bbox,start_time = datestart,end_time = dateend)
 fire_search_stac <- function(fire_bbox,
-                             start_date,
-                             end_date,
+                             start_time,
+                             end_time,
                              collection_names=c("ga_ls7e_ard_3",
                                                 "ga_ls8c_ard_3",
                                                 "ga_ls9c_ard_3",
                                                 "ga_ls5t_ard_3",
                                                 "ga_s2am_ard_3",
                                                 "ga_s2bm_ard_3")){
+
+
+  checkmate::assert(stringr::str_detect(class(start_time)[1],"POSIXct"),"Error: times must be posixct")
+  checkmate::assert(stringr::str_detect(class(end_time)[1],"POSIXct"),"Error: times must be posixct")
+
+
+  start_time <- lubridate::with_tz(start_time,tz="utc")
+  end_time <- lubridate::with_tz(end_time,tz="utc")
+
+  start_time <- format(start_time, "%Y-%m-%dT%H:%M:%SZ")
+  end_time <- format(end_time, "%Y-%m-%dT%H:%M:%SZ")
+
+
+
 
   #connect to stac source - digital earth australia
   #see information here: https://knowledge.dea.ga.gov.au/guides/setup/gis/stac/
@@ -32,7 +46,7 @@ fire_search_stac <- function(fire_bbox,
     q = stac_source,
     bbox = sf::st_bbox(sf::st_transform(fire_bbox,4326)),#which crs??
     collections = collection_names,
-    datetime = paste0(start_date,"/",end_date),
+    datetime = paste0(start_time,"/",end_time),
     limit = 999
   )
   executed_stac_query <- rstac::get_request(stac_query)
@@ -95,7 +109,7 @@ fire_search_stac <- function(fire_bbox,
 
 
     #get the time zone
-    my_tz <- fire_get_timezone(fire_bbox)
+    my_tz <- wfprogression::fire_get_timezone(fire_bbox)
 
 
     #calculate the time and image tile id fields
