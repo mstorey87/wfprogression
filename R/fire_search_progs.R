fcc1360a5c69c21a30ab277eb676a018f23216d7
Michael Storey <mstorey@uow.edu.au>
2025-01-20 12:43:23 +1100

    update fire_search_progs to accept times

--- R/fire_search_progs.R
+++ R/fire_search_progs.R
@@ -1,16 +1,28 @@
 #' Search for fire progression polygons
 #'
 #' @param fire_bbox A polygon, usually fire bounding box, to search for images
-#' @param start_date The first date for which to search for images YYYY-mm-dd
-#' @param end_date The last date for which to search for images YYYY-mm-dd
+#' @param start_time The first date for which to search for images YYYY-mm-dd
+#' @param end_time The last date for which to search for images YYYY-mm-dd
 #' @param return_geom TRUE return sf object, FALSE return data frame
 #'
 #' @return sf object
 #' @export
 #'
 #' @examples
-#' #dat.progs.sf <- fire_search_progs(fire_bbox=dat.bbox,start_date=datestart,end_date=dateend)
-fire_search_progs <- function(fire_bbox=fire_bbox_polygon(),start_date="1900-01-01",end_date="2100-01-01",return_geom=TRUE){
+#' #dat.progs.sf <- fire_search_progs(fire_bbox=dat.bbox,start_time=datestart,end_time=dateend)
+fire_search_progs <- function(fire_bbox=wfprogression::fire_bbox_polygon(),start_time,end_time,return_geom=TRUE){
+
+
+
+  checkmate::assert(stringr::str_detect(class(start_time)[1],"POSIXct"),"Error: times must be posixct")
+  checkmate::assert(stringr::str_detect(class(end_time)[1],"POSIXct"),"Error: times must be posixct")
+
+
+
+  #ensure times are local
+  my_tz <- wfprogression::fire_get_timezone(fire_bbox)
+  start_time <- lubridate::with_tz(start_time,tz=my_tz)
+  end_time <- lubridate::with_tz(end_time,tz=my_tz)
 
 
   #connect to database
@@ -28,15 +40,12 @@ fire_search_progs <- function(fire_bbox=fire_bbox_polygon(),start_date="1900-01-
     sf::st_transform(4283) %>%
     sf::st_concave_hull(ratio = 0.8)
 
-  #add times on to start and end dates
-  start_date <- paste0(start_date," 00:00:00")
-  end_date <- paste0(as.Date(end_date)+1," 00:00:00")
 
   #extract geometry of fire as text
   txt_geom <- sf::st_as_text(sf::st_geometry(fire_bbox),EWKT = T)
 
   #create date part of query
-  txt_date <- paste0("dt_local between '",start_date, "' AND '", end_date,"'")
+  txt_date <- paste0("datetimelocal between '",format(start_time,format="%Y-%m-%d %H:%M:%S"), "' AND '", format(end_time,format="%Y-%m-%d %H:%M:%S"),"'")
 
   #create whole SQL query
   #user input option to return results without geometry to save time
