c8d77d43dacf27692c9a735dfbeacc11912837cc
Michael Storey <mstorey@uow.edu.au>
2025-01-28 14:09:10 +1100

    add line coords to max spread line function

--- R/fire_max_spread_line.R
+++ R/fire_max_spread_line.R
@@ -115,6 +115,35 @@ fire_max_spread_line <- function(polygons,time_col,include_spots=F,
                       ros_kmh=line_km/timestep_hours) %>%
         dplyr::select(start_time,end_time,line_km,timestep_hours,ros_kmh)
 
+      ##add line start and end coordinates and directions
+      # Initialize results list
+      line_results <- lapply(1:nrow(dat.lines), function(i) {
+        # Extract the geometry of the current line
+        line <- sf::st_geometry(dat.lines)[i]
+
+        # Extract coordinates
+        coords <- sf::st_coordinates(line)
+
+        # Get start and end points
+        start_point <- coords[1, ]
+        end_point <- coords[nrow(coords), ]
+
+        # Calculate direction (bearing in degrees)
+        direction <- atan2(end_point[2] - start_point[2], end_point[1] - start_point[1]) * (180 / pi)
+        if (direction < 0) direction <- direction + 360
+
+        # Return as a named vector
+        return(c(
+          start_x = start_point[1], start_y = start_point[2],
+          end_x = end_point[1], end_y = end_point[2],
+          epsg=sf::st_crs(lines)$epsg,
+          direction = direction
+        ))
+      })
+
+      dat.lines <- cbind(dat.lines,line_results) %>%
+        sf::st_as_sf()
+
 
       #flag lines from results if it crosses polygons with prior times (can happen when fires merge), ie all prior times from same season (dat.prior.all)
       dat.lines.intersect <- sf::st_intersection(sf::st_geometry(dat.lines),dat.prior.all %>% dplyr::select(time_poly=time)) %>%
