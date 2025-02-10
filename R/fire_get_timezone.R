e233adc9dba80cbaf7a0612e415269fc8dc60d6d
Michael Storey <mstorey@uow.edu.au>
2025-02-10 21:31:12 +1100

    remove use of rnatural earth

--- R/fire_get_timezone.R
+++ R/fire_get_timezone.R
@@ -9,14 +9,16 @@
 #' #fire_get_timezone(dat.bbox)
 fire_get_timezone <- function(fire_bbox){
 
-  checkmate::assert(curl::has_internet(),"Error: internet connection is required")
+  #checkmate::assert(curl::has_internet(),"Error: internet connection is required")
 
 
-  closeAllConnections()
+  #closeAllConnections()
   #get the time zone based on footprint centroid and add a local time field
-  dat.aus <- rnaturalearth::ne_states(country="Australia") %>%
-    dplyr::select(name) %>%
-    sf::st_transform(3112)
+  # dat.aus <- rnaturalearth::ne_states(country="Australia") %>%
+  #   dplyr::select(name) %>%
+  #   sf::st_transform(3112)
+
+  dat.aus <- wfprogression::dat.aus
 
   dat.cent <- fire_bbox %>%
     sf::st_union() %>%
@@ -28,7 +30,7 @@ fire_get_timezone <- function(fire_bbox){
   dat.tz <-dat.aus[sf::st_nearest_feature(dat.cent,dat.aus),] %>%
     dplyr::left_join(wfprogression::dat.timezone.names,by="name")
 
-  closeAllConnections()
+  #closeAllConnections()
   return(dat.tz$tz_name)
 
 }
