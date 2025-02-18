7137b149c95ba3383760ecd73b225b471ff8bb64
Michael Storey <mstorey@uow.edu.au>
2025-02-18 15:52:38 +1100

    update image copying

--- R/fire_hotspot_map_v2.R
+++ R/fire_hotspot_map_v2.R
@@ -187,7 +187,7 @@ message("hotspots downloaded")
 
 
         if(nrow(hotspots2)==0 & add_hotspots==T){
-          file.copy(outtif2,paste0(dest_folder,"\\",basename(outtif2)),overwrite=T)
+          file.copy(outtif1,paste0(dest_folder,"\\x",basename(outtif2)),overwrite=T)
           next
         }
 
