a69f2503523904200459231382b12e8683a99c18
mstorey87 <michaelstorey@y7mail.com>
2025-11-07 20:38:16 +1100

    update load sam

--- R/fire_load_sam_once.R
+++ R/fire_load_sam_once.R
@@ -15,17 +15,29 @@ fire_load_sam_once <- function() {
   if (!.fire_env$sam_loaded) {
 
     message("loading SAM module")
-    reticulate::use_python("C:/Users/mstorey/AppData/Local/Programs/Python/Python313/python.exe", required = TRUE)
-
+    #reticulate::use_python("C:/Users/mstorey/AppData/Local/Programs/Python/Python313/python.exe", required = TRUE)
+    reticulate::use_condaenv(condaenv = "sam2_env")
     reticulate::py_run_string("
 import torch
 import numpy as np
 import cv2
+import os
+import sys
+
 from sam2.build_sam import build_sam2
 from sam2.sam2_image_predictor import SAM2ImagePredictor
 
-sam2_checkpoint = r'C:\\Users\\mstorey\\sam2\\checkpoints\\sam2.1_hiera_base_plus.pt'
-model_cfg = r'C:\\Users\\mstorey\\sam2\\checkpoints\\sam2.1_hiera_b+.yaml'
+
+# --- Find the conda environment folder ---
+conda_prefix = sys.prefix
+checkpoints_dir = os.path.join(conda_prefix, 'checkpoints')
+
+# --- Define model paths ---
+sam2_checkpoint = os.path.join(checkpoints_dir, 'sam2.1_hiera_tiny.pt')
+model_cfg = os.path.join(checkpoints_dir, 'sam2.1_hiera_tiny.yaml')
+
+#sam2_checkpoint = r'C:\\Users\\mstorey\\sam2\\checkpoints\\sam2.1_hiera_base_plus.pt'
+#model_cfg = r'C:\\Users\\mstorey\\sam2\\checkpoints\\sam2.1_hiera_b+.yaml'
 
 if torch.cuda.is_available():
     device = torch.device('cuda')
