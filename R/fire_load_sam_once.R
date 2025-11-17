48c369698bb1df6e3c88678510f85b3a43af8976
mstorey87 <michaelstorey@y7mail.com>
2025-11-17 13:03:42 +1100

    v

--- R/fire_load_sam_once.R
+++ R/fire_load_sam_once.R
@@ -7,20 +7,17 @@
 
 #' Internal: Load SAM model once
 #' @param checkpoints_dir Directory with model checkpoint pt and config yaml
-#' @param docker TRUE or FALSE if building to docker image
 #'
 #' Loads the Python SAM2 model using reticulate. This runs only once per R session.
 #' It sets a flag in a package-private environment.
 #'
 #' @return Invisibly returns TRUE.
 #' @noRd
-fire_load_sam_once <- function(checkpoints_dir=NULL,docker=FALSE) {
+fire_load_sam_once <- function(checkpoints_dir=NULL) {
   if (!.fire_env$sam_loaded) {
 
     message("loading SAM module")
-    #reticulate::use_python("C:/Users/mstorey/AppData/Local/Programs/Python/Python313/python.exe", required = TRUE)
-    #reticulate::use_condaenv(condaenv = "sam2_env")
-    #reticulate::py$checkpoints_dir <- checkpoints_dir
+
 
     if(is.null(checkpoints_dir)){
       checkpoints_dir <- tempdir()
@@ -47,8 +44,7 @@ fire_load_sam_once <- function(checkpoints_dir=NULL,docker=FALSE) {
                       sam2_config_loc,
                       mode = "wb")
 
-          #reticulate::py$model_cfg <- sam2_config_loc
-          #reticulate::py$sam2_checkpoint <- sam2_checkpoint_loc
+
           reticulate::py_run_string(glue::glue("
 model_cfg = r'{sam2_config_loc}'
 sam2_checkpoint = r'{sam2_checkpoint_loc}'
@@ -60,13 +56,8 @@ sam2_checkpoint = r'{sam2_checkpoint_loc}'
 model_cfg = r'{sam2_config}'
 sam2_checkpoint = r'{sam2_checkpoint}'
 "))
-      # reticulate::py$model_cfg <- sam2_config
-      # reticulate::py$sam2_checkpoint <- sam2_checkpoint
-
 
     }
-
-
 
     reticulate::py_run_string("
 import torch
@@ -88,106 +79,12 @@ else:
 
 ")
 
-    if(docker==FALSE){
+
           reticulate::py_run_string("
 sam2_model = build_sam2(model_cfg, sam2_checkpoint, device=device)
 predictor = SAM2ImagePredictor(sam2_model)
 ")
 
-    }else{
-
-      reticulate::py_run_string(glue::glue("
-import os
-import shutil
-import sam2
-
-# --- Source and destination for YAML ---
-src_yaml = r'{sam2_config}'
-dest_dir = os.path.join(os.path.dirname(sam2.__file__), 'configs', 'sam2.1')
-os.makedirs(dest_dir, exist_ok=True)
-dest_yaml = os.path.join(dest_dir, 'sam2.1_hiera_t.yaml')
-
-# --- Copy YAML into package folder ---
-shutil.copy2(src_yaml, dest_yaml)
-print('Copied YAML to:', dest_yaml)
-
-# --- Set Hydra config path to the copied YAML directory ---
-os.environ['HYDRA_CONFIG_PATH'] = os.path.dirname(dest_yaml)
-os.environ['HYDRA_FULL_ERROR'] = '1'
-
-# --- Build SAM2 model using the copied YAML ---
-checkpoint = r'{sam2_checkpoint}'
-config_file = 'sam2.1_hiera_t.yaml'  # relative to Hydra config path
-sam2_model = sam2.build_sam2(config_file=config_file, checkpoint=checkpoint, device='cpu')
-predictor = sam2.SAM2ImagePredictor(sam2_model)
-
-print('SAM2 model loaded successfully in Docker.')
-"))
-
-# Access from R
-# predictor <- reticulate::py$predictor
-# sam2_model <- reticulate::py$sam2_model
-
-
-    }
-
-
-#       reticulate::py_run_string(glue::glue("
-# # --- Create custom Hydra config path ---
-# custom_cfg_dir = r'{checkpoints_dir}'
-# os.environ['HYDRA_CONFIG_PATH'] = custom_cfg_dir
-# os.environ['HYDRA_FULL_ERROR'] = '1'
-#
-# # if r'{checkpoints_dir}' not in sys.path:
-# #     sys.path.append(r'{checkpoints_dir}')
-# #print(f'Added config path to sys.path: {checkpoints_dir}')
-#
-# # --- Build SAM2 model ---
-# config_file = r'sam2.1/sam2.1_hiera_t.yaml'  # relative to the custom config folder
-# checkpoint = r'{file.path(checkpoints_dir, 'sam2.1_hiera_tiny.pt')}'
-#
-# sam2_model = build_sam2(
-#     config_file=config_file,  # relative to sam2 package
-#     checkpoint=checkpoint,
-#     device=device
-# )
-#
-# predictor = SAM2ImagePredictor(sam2_model)
-# print('SAM2 model loaded successfully.')"))
-
-
-#       reticulate::py_run_string(glue::glue("
-# import shutil
-# import sam2
-#
-# # --- Paths --- Need to copy to config path on decoker
-# src_yaml = r'{file.path(checkpoints_dir, 'sam2.1_hiera_t.yaml')}'
-# dest_dir = os.path.join(os.path.dirname(sam2.__file__), 'configs', 'sam2.1')
-# #dest_dir = os.path.join(os.path.dirname(sam2.__file__))
-# #os.makedirs(dest_dir, exist_ok=True)
-# dest_yaml = os.path.join(dest_dir, 'sam2.1_hiera_t.yaml')
-#
-# # --- Copy YAML into package ---
-# shutil.copy2(src_yaml, dest_yaml)
-# print('Not Copied YAML to:', dest_yaml)
-#
-# checkpoint = r'{file.path(checkpoints_dir, 'sam2.1_hiera_tiny.pt')}'
-#
-# config_path_rel = 'configs/sam2.1/sam2.1_hiera_t.yaml'
-# #config_path_rel = 'sam2.1_hiera_t.yaml'
-# print('Entering YAML path to SAM:', config_path_rel)
-# # --- Build SAM2 model ---
-# sam2_model = build_sam2(
-#     config_file=config_path_rel,  # relative to sam2 package
-#     checkpoint=checkpoint,
-#     device=device
-# )
-#
-# predictor = SAM2ImagePredictor(sam2_model)
-# print('SAM2 model loaded successfully.')
-# "))
-
- #   }
 
     .fire_env$sam_loaded <- TRUE
   }
